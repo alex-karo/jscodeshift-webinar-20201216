270b74564a63117617f5568df52b55a2a50516f8
Alex Karo <alex@karo-dev.ru>
2020-12-16 18:51:32 +0300

    Args to object

--- args-to-object/args-to-object.ts
+++ args-to-object/args-to-object.ts
@@ -7,7 +7,37 @@ const transform: Transform = (file, api, options) => {
   const root = j(source);
   let functionName;
 
-  // TODO
+  root.find(j.ImportDeclaration)
+    .filter(({ value }) => {
+      return typeof value.source.value === 'string'
+        && value.source.value.includes('/lib/format');
+    })
+    .forEach(path => {
+      const specifier = j(path).find(j.ImportSpecifier, {imported: { name: 'format' }}).paths()[0];
+      functionName = specifier.value.local.name;
+    });
+
+  if (!functionName) {
+    root.toSource();
+  }
+
+  root.find(j.CallExpression, { callee: { name: functionName } })
+    .forEach(path => {
+      const formatArgs = path.value.arguments;
+      const objectParam = j.objectExpression(
+        formatArgs.map((expression, idx) => {
+          if (j.SpreadElement.check(expression)) {
+            throw Error('Spread not supported');
+          }
+          return j.objectProperty.from({
+            key: j.identifier(funcArgs[idx]),
+            value: expression,
+          })
+        })
+      );
+      const newFunc = j.callExpression(path.value.callee, [objectParam]);
+      j(path).replaceWith(newFunc);
+    })
 
   return root.toSource();
 }
